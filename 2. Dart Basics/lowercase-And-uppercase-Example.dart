105c66b9b3c44320f9e7f123d89f62eae76d8822
Kunal Kumar <kunal.kumar@progcap.com>
2021-11-08 13:07:56 +0530

    Added two folders

---
+++ 2. Dart Basics/lowercase-And-uppercase-Example.dart
@@ -0,0 +1,7 @@
+void main(){
+  String name = 'Kunal Kumar';
+  String title = 'Software Enginner';
+  print(name.toUpperCase());
+  print(title.toLowerCase());
+  print('I am $name.toUpperCase() and I am $title.toLowerCase()');
+}
